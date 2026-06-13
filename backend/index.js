b666bd021330de3983b2834f2252a1217b0db741
Jain CJ <jain@Jains-Air.lan>
2026-06-13 22:09:25 +0530

    Bug Fixes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/jobs', jobRouter);
+app.use('/api/jobs', require('./routes/jobs'));
 
 
 // 2. Configure CORS to explicitly trust your Vercel frontend
